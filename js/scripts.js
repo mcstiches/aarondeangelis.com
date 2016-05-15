d3bcb9e3aff94528546bd4e9669d26ba363ad0c7
Aaron DeAngelis <deangeaj@mountunion.edu>
2016-05-14 21:03:48 -0700

    nav bar and content

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+<script>
+    jQuery(document).ready(function($) {
+        $('.my-slider').unslider({
+            infinite: true,
+            autoplay: true, 
+            arrows: false
+        });
+    });
+</script>
+
+
+
